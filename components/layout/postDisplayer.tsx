948c9b041e499991046d40f1a256af3d6bd16b6a
Stan.B <167423087+Coundelitche@users.noreply.github.com>
2025-03-05 11:03:16 +0700

    Update postDisplayer.tsx

--- components/layout/postDisplayer.tsx
+++ components/layout/postDisplayer.tsx
@@ -1,13 +1,25 @@
 import { getPost } from "@/app/action/post";
 import { PostPreviewCard } from "../post/postPreviewCard";
 
+interface Post {
+  id: string;
+  title: string;
+  description: string;
+  content: string;
+  imageUrl: string;
+  author: {
+    id: string;
+    name: string | null;
+  };
+}
+
 export const PostDisplayer = async () => {
   const posts = await getPost();
   return (
     <div className="flex flex-col items-center gap-4 py-4">
       <h2 className="text-center text-2xl">Latest Posts</h2>
       <div className="px-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {posts.map((post) => {
+        {posts.map((post: Post) => {
           return <PostPreviewCard key={post.id} post={post} />;
         })}
       </div>
